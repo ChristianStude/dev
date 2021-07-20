18153e15a12aedd7e186975a0daf27cba50d3ea8
Christian Stude <christian.stude@googlemail.com>
2021-07-20 11:15:26 +0200

    kleine Änderungen

--- playground/quadrate auflisten.py
+++ playground/quadrate auflisten.py
@@ -1,5 +1,9 @@
-n = 10
+n = input("Bitte eine Zahl eingeben.")
 
 while n > 0:
     print("Quadrat von", n, "ist", n ** 2 )
     n -= 1 # n = n - 1
+    if n == 5:
+        break
+else:
+    print:("while else?")
