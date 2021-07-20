a8c08623cabb112fc006a574217fdd368d1f056c
Christian Stude <christian.stude@googlemail.com>
2021-07-20 11:08:17 +0200

    while Schleife hinzugefügt und kleine Änderungen

--- playground/gehaltstyp.py
+++ playground/gehaltstyp.py
@@ -2,16 +2,18 @@
 # Wenn das Gehalt kleiner gleich  450,- €, dann gib MiniJobber aus, wenn kleiner gleich 1.000,- € ist
 # dan gib Billiglohnarbeiter aus, wenn kleiner gleich 2.000,- € dann Normalverdiener ausgeben.
 # Wenn das Gehalt größer als 8.000,- € dann Spitzennverdiener ausgeben.
+while 1 == 1:
+    gehalt = int(input("Bitte geben Sie die Höhe Ihres Geheltes ein!"))
 
-gehalt = int(input("Bitte geben Sie die Höhe Ihres Geheltes ein!"))
-
-if gehalt <= 450:
-    print("MiniJobber")
-elif gehalt <= 1_000:
-    print("Billiglohnarbeiter")
-elif gehalt > 1_000 and gehalt <= 7_999:
-    print("Normalverdiener")
-elif gehalt >= 8_000:
-    print("Herzlichen Glückwunsch, Sie sind ein Spitzenverdiener.")
-else:
-    print("keine Ahnung")
+    if gehalt <= 450:
+        print("MiniJobber")
+    elif gehalt <= 1_000:
+        print("Billiglohnarbeiter")
+    elif gehalt > 1_000 and gehalt <= 7_999:
+        print("Normalverdiener")
+    elif gehalt >= 8_000:
+        print("Herzlichen Glückwunsch, Sie sind ein Spitzenverdiener.")
+    else:
+        print("keine Ahnung")
+    input("Bitte Taste drücken")
+    
---
+++ playground/while_schleife.py
@@ -0,0 +1,12 @@
+# while bedingung
+#    anweisung
+#    anweisung
+
+while 1==1:
+    eingabe = input("$")
+    print(eingabe)
+    continue
+    if eingabe == "":
+        break
+
+print("Programmende")
