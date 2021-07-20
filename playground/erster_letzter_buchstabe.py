1267a8cdba4678acc130f75a2240f24a2da0ea67
Christian Stude <christian.stude@googlemail.com>
2021-07-20 08:31:06 +0200

    erster und letzter Buchstabe hinzugefügt

--- playground/erster_letzter_buchstabe.py
+++ playground/erster_letzter_buchstabe.py
@@ -1,4 +1,5 @@
 eingabe = input("Zeichenkette eingeben:")
+print("Vierfache Eingabe in groß:", 4 * eingabe.upper())
 
 if len(eingabe) >=2:
     print(eingabe[0], eingabe[1], eingabe[-2], eingabe[-1])
