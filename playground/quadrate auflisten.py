a0d6cb2b1a3528b7ab5dccbef4bf77538726f8e8
Christian Stude <christian.stude@googlemail.com>
2021-07-21 09:03:26 +0200

    while in quadrate implementiert

--- playground/quadrate auflisten.py
+++ playground/quadrate auflisten.py
@@ -1,9 +1,10 @@
-n = int(input("Bitte eine Zahl eingeben."))
+while 1 == 1:
+    n = int(input("Bitte eine Zahl eingeben."))
 
-while n > 0:
-    print("Quadrat von", n, "ist", n ** 2 )
-    n -= 1 # n = n - 1
-    if n == 5:
-        break
-else:
-    print:("Ende")
+    while n > 0:
+        print("Quadrat von", n, "ist", n ** 2 )
+        n -= 1 # n = n - 1
+        #if n == 5:
+            #break
+    else:
+        print:("Ende")
