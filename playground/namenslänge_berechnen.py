aaf9eacb5a3a21c9b237d9307fa0ccb665a0c594
Christian Stude <christian.stude@googlemail.com>
2021-07-22 07:53:51 +0200

    Namenslänge berechnen hinzugefügt

---
+++ playground/namenslänge_berechnen.py
@@ -0,0 +1,6 @@
+# Erzeuge neue Liste aus Namen, wobei die Elemente den Längen der einzelnen Namen entsprechen.
+
+namen =["Hans", "Herbert", "Rosi", "Norbert", "Hubertus"]
+
+print(list(map(len, namen)))
+
