cd651204385830581bc7b470753089e1a952f2ec
Christian Stude <christian.stude@googlemail.com>
2021-07-22 10:21:49 +0200

    logische Operatoren angelegt

---
+++ playground/logische_operatoren.py
@@ -0,0 +1,16 @@
+namen = [
+    "stephan",
+    "mathias",
+    "christian",
+    "dyea",
+    "norbert",
+    "hubertus"
+]
+
+#for name in namen:
+#    if (name[-1] == "n") or (name[-1] =="s"):
+#        print(name)
+
+for name in namen:
+    if (name[0] == "s") and (name[-2] == "a"):
+        print(name)
