78cd9d6aa2cfcc03d721594b7a8475bcdfdc3043
Christian Stude <christian.stude@googlemail.com>
2021-07-22 13:49:50 +0200

    Tool angepasst

--- playground/tool.py
+++ playground/tool.py
@@ -1,3 +1,5 @@
+import json
+
 # Lasse den Nutzer solange eine Eingabe tätigen, bis die Eingabe einer der vorgegebenen Möglichkeiten entspricht.
 def leseEingabe(prompt, eingabeOptionen):
     while True:
@@ -11,17 +13,8 @@ def gibAuswahloptionenAus(auswahlOptionen):
     for index, option in enumerate(auswahlOptionen):
         print(index + 1, ": ", option, sep="")
 
-def berechneSummeUndProdukt (a, b):
-    summe = a + b
-    produkt = a * b
-    return (summe, produkt)
-
-#gibAuswahloptionenAus(["Stephan", "Mathias", "Christian", "Dyea", "Norbert"])
-#leseEingabe("Eingabe (1-5):", ["1", "2", "3", "4", "5"])
-
-#leseEingabe("Eingabe (A,B,C,D):", ["a", "b", "c", "d"])
-
-#leseEingabe("Eingabe: ja (j), nein (n), vielleicht (v):", ["j", "n", "v"])
-
-#x, y = berechneSummeUndProdukt(2, 4)
-#print(x, y)
+def LadeFragenliste(pfad):
+    fragenDatei = open(pfad, encoding="utf-8")
+    fragenListe = json.load(fragenDatei)
+    fragenDatei.close()
+    return fragenListe
