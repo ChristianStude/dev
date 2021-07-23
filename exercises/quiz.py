d4ddca200a5760b97fb3c01bbec2e049b4b4c2c3
Christian Stude <christian.stude@googlemail.com>
2021-07-23 10:26:33 +0200

    kleine Änderungen

--- exercises/quiz.py
+++ exercises/quiz.py
@@ -9,13 +9,14 @@ anzahlPunkte = 0
 for aufgabe in fragenListe:
     print(aufgabe["frage"])
     utils.tool.gibAuswahloptionenAus(aufgabe["antworten"])
-    eingabe = int(utils.tool.leseEingabe("Eingabe 1-4:", ["1", "2", "3", "4"]))
+    eingabe = int(utils.tool.leseEingabe("Bitte gib eine der Antwortmöglichkeiten ein: ", ["1", "2", "3", "4"]))
     if eingabe == aufgabe["lösung"]:
         anzahlPunkte = anzahlPunkte + 1
-        print("Richtig!")
+        print("Das war Richtig!")
+
     else:
         print("Das war leider falsch.")
 
-print("Du hast", anzahlPunkte, "erreicht!")
+print("Du hast", anzahlPunkte, "Punkte erreicht!")
 
 
